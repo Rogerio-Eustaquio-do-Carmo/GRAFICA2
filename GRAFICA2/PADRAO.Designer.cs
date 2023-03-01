ea47a73a363a89ebfa2192b86e875dcaadc23e84
Rogerio <rogerioeustaquio@hotmail.com>
2023-03-01 06:17:46 -0300

    ADICIONANDO OS MENUSTRIP

--- GRAFICA2/PADRAO.Designer.cs
+++ GRAFICA2/PADRAO.Designer.cs
@@ -30,6 +30,9 @@ namespace GRAFICA2
         private void InitializeComponent()
         {
             this.CBBOPCOES = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip3 = new System.Windows.Forms.MenuStrip();
             this.SuspendLayout();
             // 
             // CBBOPCOES
@@ -39,10 +42,34 @@ namespace GRAFICA2
             "CADASTRAR",
             "ATUALIZAR",
             "EXCLUIR"});
-            this.CBBOPCOES.Location = new System.Drawing.Point(356, 63);
+            this.CBBOPCOES.Location = new System.Drawing.Point(307, 98);
             this.CBBOPCOES.Name = "CBBOPCOES";
             this.CBBOPCOES.Size = new System.Drawing.Size(169, 21);
             this.CBBOPCOES.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 48);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1784, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1784, 24);
+            this.menuStrip2.TabIndex = 2;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // menuStrip3
+            // 
+            this.menuStrip3.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip3.Name = "menuStrip3";
+            this.menuStrip3.Size = new System.Drawing.Size(1784, 24);
+            this.menuStrip3.TabIndex = 3;
+            this.menuStrip3.Text = "menuStrip3";
             // 
             // PADRAO
             // 
@@ -51,16 +78,24 @@ namespace GRAFICA2
             this.BackColor = System.Drawing.SystemColors.Info;
             this.ClientSize = new System.Drawing.Size(1784, 861);
             this.Controls.Add(this.CBBOPCOES);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.Controls.Add(this.menuStrip3);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "PADRAO";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ComboBox CBBOPCOES;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.MenuStrip menuStrip3;
     }
 }
 
